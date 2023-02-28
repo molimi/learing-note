fc6c0ed034ea03e51a051f30e82ccb036d76fa9f
CarpeDiem2021 <qxie8888@gmail.com>
2023-02-28 22:28:48 +0800

    230228 exercise

---
+++ 2022/algorithm/Tree/treelist.py
@@ -0,0 +1,53 @@
+#! /user/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+@author: CarpeDiem
+@date: 23/2/27
+@version: 0.1
+@description: 树的列表实现
+"""
+
+def binary_tree(r):
+    return [r, [], []]
+
+def insert_left(root, new_branch):
+    t = root.pop(1)     # 取出左子树
+    if len(t) > 1:      # 左子树已存在
+        root.insert(1, [new_branch, t, []])
+    else:
+        root.insert(1, [new_branch, [], []])
+    return root
+
+def insert_right(root, new_branch):
+    t = root.pop(2)     # 取出右子树
+    if len(t) > 1:      # 右子树已存在
+        root.insert(2, [new_branch, [], t])
+    else:
+        root.insert(2, [new_branch, [], []])
+    return root
+    
+def get_root_val(root):
+    return root[0]
+
+def set_root_val(root, new_val):
+    root[0] = new_val
+
+def get_left_child(root):
+    return root[1]
+
+def get_right_child(root):
+    return root[2]
+
+r = binary_tree(3)
+insert_left(r, 4)
+insert_left(r, 5)
+insert_right(r, 6)
+insert_right(r, 7)
+print(r)
+l = get_left_child(r)
+print(l)
+set_root_val(l, 9)
+print(r)
+insert_left(l, 11)
+print(r)
+print(get_right_child(get_right_child(r)))
