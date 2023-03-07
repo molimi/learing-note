2fafcf8c993f7535a1ddbdcca3cba1b1f2ffaa13
CarpeDiem2021 <qxie8888@gmail.com>
2023-03-07 22:40:23 +0800

    230307 exercise

--- 2022/LeetCode/Code/2303/75.颜色分类.py
+++ 2022/LeetCode/Code/2303/75.颜色分类.py
@@ -2,7 +2,8 @@
 # @lc app=leetcode.cn id=75 lang=python3
 #
 # [75] 颜色分类
-#
+# 思路一：统计0和1出现的次数，直接把数组重新赋值
+# 思路二：单指针，依次交换位置
 
 # @lc code=start
 class Solution:
@@ -10,5 +11,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        i, p0, p1 = 0, 0 , len(nums)-1
+        while i <= p1:
+            if nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                p0 += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 -= 1
+            else:
+                i += 1
+
 # @lc code=end
 
