9db1a5f12c13b0a0a5e46fef65e6498f91db7399
CarpeDiem2021 <qxie8888@gmail.com>
2023-03-14 22:09:16 +0800

    230314 exercise

--- 2022/LeetCode/Code/2303/148.排序链表.py
+++ 2022/LeetCode/Code/2303/148.排序链表.py
@@ -11,58 +11,24 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]: 
-        if not head:        # 空链表直接返回
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        return self.split_list(head) 
+    
+    def split_list(self, head: ListNode):   
+        '''处理归并排序的边界问题,下面两种场景都不需要进行分合操作'''
+        if not head or not head.next:
             return head
-        
-        length = 0          # 获取链表的长度
-        node = head
-        while node: 
-            length += 1
-            node = node.next
-        
-        dummy_head = ListNode(0, head)
-        sub_length = 1                                # 归并的有效处理长度，最小为 1
-        while sub_length < length:
-            prev, curr = dummy_head, dummy_head.next  # 为了更好的实现迭代，定义 prev, curr 分别指向 dummy_head, dummy_head.next
-            while curr:                               # 当 cur 不为空时
-                head1 = curr                           # 定义当前链表，head1 指向 cur
-                for i in range(1, sub_length):
-                    '''当 i 等于 1 时, cur 不需要指向后下一个链表其余情况，
-                       cur 在链表 cur 不为空的情况, 向后移动 sub_length - 1 个位置'''
-                    if curr.next:
-                        curr = curr.next
-                    else:
-                        break
-                head2 = curr.next   # 切断链表，第一部分长度为 sub_length
-                curr.next = None    # 第二部分为 head 除 head1 以外的部分
-                curr = head2
-                for i in range(1, sub_length):  # 将 cur 在满足 cur.next 不为空的情况下， 又往后移动 sub_length 长度
-                    if curr and curr.next:
-                        curr = curr.next
-                    else:
-                        break
-                
-                remain = None         # 定义 remain 链表，指向 null
-                if curr:              # 当移动完 sub_length 后，仍不为空，remain 等于 cur.next;
-                    remain = curr.next
-                    curr.next = None  # 切断链表， 这时候 head2 的长度与 head1 一致
-                
-                merged = self.merge(head1, head2)   # merge , 然后 prev 的 next 指针指向将 merge 后的子链表
-                prev.next = merged
-                while prev.next:        # 然后将 prev 指向与 merge 完成后的链表位置
-                    prev = prev.next
-                curr = remain           # 然后当前节点位置指向 remain 部分链表
-            '''
-            进入下一批次的归并排序操作，直到将相同 sub_length 的全部处理完，才会退出此处 while
-            再进入到下轮 sub_length, sub_length 以 1 -> 2 -> 4 -> 8 的方法进行，符合自低向上不断迭代，
-            直到找到最终答案。其实递归底层本质也是一样的，到最短的1，才开始合并，不断合并，到最终结果
-            '''
-            sub_length <<= 1
+        prev, slow, fast = head, head, head
+        while fast and fast.next:   # 找中点,偶数找的后面那个中点的位置，奇数找到中点
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = None
+        left, right = head, slow     # 将链表分割成两个子链表
+        return self.merge_sort(self.split_list(left), self.split_list(right))    # 分割完后，进行合并部分操作
 
-        
-        return dummy_head.next
-    def merge(self, head1: ListNode, head2: ListNode) -> ListNode:
+    def merge_sort(self, head1: ListNode, head2: ListNode) -> ListNode:
+        '''归并环节'''
         dummy_head = ListNode(0)            # 构建虚拟头结点
         temp, temp1, temp2 = dummy_head, head1, head2
         while temp1 and temp2:              # 开始合并操作
--- 2022/LeetCode/Code/2303/23.合并k个升序链表.py
+++ 2022/LeetCode/Code/2303/23.合并k个升序链表.py
@@ -2,7 +2,7 @@
 # @lc app=leetcode.cn id=23 lang=python3
 #
 # [23] 合并K个升序链表
-#
+# 两个两个合并
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -10,7 +10,71 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
+import heapq 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        head = lists[0]
+        for item in lists[1:]:
+            head = self.mergeTwoLists(head, item)
+        return head
+    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prehead = ListNode(-1)  # 哨兵节点
+        prev = prehead          # 指针节点
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        prev.next = list1 if list1 else list2
+        return prehead.next
+
+        priority_queue = []
+        for llist in lists: 
+            while llist:
+                heapq.heappush(priority_queue, llist.val)       # 把llist中的数据逐个加到堆中
+                llist = llist.next
+        dummy = ListNode(0)                                     # 构造虚节点
+        p = dummy
+        while priority_queue:
+            p.next = ListNode(heapq.heappop(priority_queue))    # 依次弹出最小堆的数据
+            p = p.next
+        return dummy.next 
+'''
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists: return None
+        n = len(lists)                          # 记录子链表数量
+        return self.mergeSort(lists, 0, n - 1)  # 调用归并排序函数
+    
+    def mergeSort(self, lists: List[ListNode], l: int, r: int) -> ListNode:
+        if l == r:
+            return lists[l]
+        m = (l + r) // 2
+        L = self.mergeSort(lists, l, m)         # 循环的递归部分
+        R = self.mergeSort(lists, m + 1, r)
+        return self.mergeTwoLists(L, R)         # 调用两链表合并函数
+    
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prehead = ListNode(-1)  # 哨兵节点
+        prev = prehead          # 指针节点
+        while list1 and list2:
+            if list1.val <= list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        prev.next = list1 if list1 else list2
+        return prehead.next
+
+
 # @lc code=end
 
